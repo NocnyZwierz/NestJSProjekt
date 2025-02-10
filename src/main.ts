31daf70342debfb40df4ab82ea1f821a81dd3a97
Paweł <p.grabczak@cognitionhub.com>
2025-02-10 16:02:36 +0100

    Module 34.3

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { LoggerInterceptor } from './shared/interceptors/logger.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalInterceptors(new LoggerInterceptor());
+  app.setGlobalPrefix('api');
+  await app.listen(8000);
 }
 bootstrap();
--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,13 +1,37 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { db, Order } from './../db';
+import { v4 as uuidv4 } from 'uuid';
+import { CreateOrderDTO } from './../products/dtos/create-order.dto';
+import { UpdateOrderDTO } from 'src/products/dtos/update-order.dto';
 
 @Injectable()
 export class OrdersService {
+  updateById(id: string, orderData: UpdateOrderDTO) {
+    throw new Error('Method not implemented.');
+  }
   public getAll(): Order[] {
     return db.orders;
   }
 
   public getById(id: Order['id']): Order | null {
-    return db.orders.find((p) => p.id === id);
+    return db.orders.find((o) => o.id === id);
   }
+
+  public deleteById(id: Order['id']): void {
+    db.orders = db.orders.filter((o) => o.id !== id);
+  }
+
+  public create(orderData: CreateOrderDTO): Order {
+    const newOrder: Order = {
+      id: uuidv4(),
+      client: 'Default Client',
+      address: 'Default Address',
+      productId: orderData.items[0].productId,
+    };
+  
+    db.orders.push(newOrder);
+    return newOrder;
+  }
+  
 }
